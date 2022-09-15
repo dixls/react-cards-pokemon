dc0639aa1b2a3256050d8fc0b1d99ba20610cb01
pixls <tha.pixls@gmail.com>
2022-09-15 13:52:06 -0400

    update useAxios to work with passed args
    refactored pokedex to use useAxios

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -4,7 +4,9 @@ import uuid from "uuid";
 
 const useAxios = (url) => {
     const [cards, setCards] = useState([]);
-    const addCard = async () => {
+    const addCard = async (name) => {
+        console.log(name);
+        if(typeof(name) == "string") url = url+name+"/";
         const res = await Axios.get(url);
         setCards(cards => [...cards, { ...res.data, id: uuid() }]);
     }
